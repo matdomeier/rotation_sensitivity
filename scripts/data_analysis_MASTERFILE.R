f13268a32a9973f040810dca26249a4511dfd44a
Buffan3369 <94531347+Buffan3369@users.noreply.github.com>
2022-02-23 14:56:59 +0100

    changed script directories

--- scripts/data_analysis_MASTERFILE.R
+++ scripts/data_analysis_MASTERFILE.R
@@ -3,25 +3,25 @@
 
 
 ## Import model's polygons as shapefiles and proceed to the georeferencing ----------------------------
-source("./R/data_analysis/georeferencing_and_NA_pos.R")
+source("./scripts/data_analysis/georeferencing_and_NA_pos.R")
 
 
 
 ## COMPARISON ------------------------------------------------------------------------------------------
 #we scale all models on the same grid-cells (maximise the get_na_pos() function for all models of "models")
 #Loop to return the indexes of the NA of the models having the smaller coverage (hence the maximal number of cells with no attribute)
-source("./R/data_analysis/cells_to_drop.R")
+source("./scripts/data_analysis/cells_to_drop.R")
 
 
 
 ## Latitude standard deviation -------------------------------------------------------------------------
-source("./R/data_analysis/lat_sd.R")
+source("./scripts/data_analysis/lat_sd.R")
 
 
 
 ## Plate ID assignment discrepancies --------------------------------------------------------------------
-source("./R/data_analysis/ID_weight.R") #generates the dataframe containing the ID_weights for all our cells
+source("./scripts/data_analysis/ID_weight.R") #generates the dataframe containing the ID_weights for all our cells
 
 
 ## Latitude deviation (between outputs 2 by 2) ----------------------------------------------------------
-source("./R/data_analysis/lat_dev_2_by_2.R")
+source("./scripts/data_analysis/lat_dev_2_by_2.R")
--- scripts/visualisation_MASTERFILE.R
+++ scripts/visualisation_MASTERFILE.R
@@ -3,18 +3,18 @@
 
 
 ## set the background map -------------------------------------------------------------
-source("./R/visualisation/background_map.R") #ignore warnings
+source("./scripts/visualisation/background_map.R") #ignore warnings
 
 
 ## Plot and save latitude standard deviation time series ------------------------------
-source("./R/visualisation/plot_lat_sd.R") #idem
+source("./scripts/visualisation/plot_lat_sd.R") #idem
 
 
 ## Plot and save latitude deviation 2 by 2 for all the models time series -------------
-source("./R/visualisation/plot_lat_deviation.R")
+source("./scripts/visualisation/plot_lat_deviation.R")
     ### FOR THE SCRIPT TO WRITE THE GIFS, SEE THE PYTHON NOTEBOOK "make_GIFs.ipynb"
 
 
 ## Plot and save plateIDs assignement discrepancies -----------------------------------
-source("./R/visualisation/plot_ID_weight.R")
+source("./scripts/visualisation/plot_ID_weight.R")
 
