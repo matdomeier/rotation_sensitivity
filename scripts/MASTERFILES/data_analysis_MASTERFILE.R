cd7fdf9f28fa26a072aa9c8f4e39da778edc3baa
Buffan3369 <94531347+Buffan3369@users.noreply.github.com>
2022-02-25 17:11:15 +0100

    I like to move it move it

--- scripts/MASTERFILES/data_analysis_MASTERFILE.R
+++ scripts/MASTERFILES/data_analysis_MASTERFILE.R
@@ -3,13 +3,11 @@
 
 
 ## Import model's polygons as shapefiles and proceed to the georeferencing ----------------------------
-source("./scripts/data_analysis/georeferencing_and_NA_pos.R")
-
+source("./scripts/data_analysis/georeferencing_and_NA_pos.R") #georeferencing
+source("./scripts/data_analysis/cells_to_drop.R") #spatial scaling of the ourputs of the models
 
 
 ## COMPARISON ------------------------------------------------------------------------------------------
-source("./scripts/data_analysis/cells_to_drop.R") #spatial scaling of the ourputs of the models
-
 source("./scripts/data_analysis/lat_sd.R") # Latitude standard deviation
 source("./scripts/data_analysis/lat_dev_2_by_2.R") # Latitude deviation (between outputs 2 by 2)
 
