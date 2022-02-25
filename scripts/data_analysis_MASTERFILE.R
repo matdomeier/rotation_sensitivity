c2c99bbd1460a1c49cc4947b7cf98068d70a1189
Buffan3369 <94531347+Buffan3369@users.noreply.github.com>
2022-02-25 15:55:45 +0100

    modified comments

--- scripts/data_analysis_MASTERFILE.R
+++ scripts/data_analysis_MASTERFILE.R
@@ -8,20 +8,11 @@ source("./scripts/data_analysis/georeferencing_and_NA_pos.R")
 
 
 ## COMPARISON ------------------------------------------------------------------------------------------
-#we scale all models on the same grid-cells (maximise the get_na_pos() function for all models of "models")
-#Loop to return the indexes of the NA of the models having the smaller coverage (hence the maximal number of cells with no attribute)
-source("./scripts/data_analysis/cells_to_drop.R")
+source("./scripts/data_analysis/cells_to_drop.R") #spatial scaling of the ourputs of the models
+
+source("./scripts/data_analysis/lat_sd.R") # Latitude standard deviation
+source("./scripts/data_analysis/lat_dev_2_by_2.R") # Latitude deviation (between outputs 2 by 2)
+
+source("./scripts/data_analysis/ID_weight.R") # Plate ID assignment discrepancies
 
 
-
-## Latitude standard deviation -------------------------------------------------------------------------
-source("./scripts/data_analysis/lat_sd.R")
-
-
-
-## Plate ID assignment discrepancies --------------------------------------------------------------------
-source("./scripts/data_analysis/ID_weight.R") #generates the dataframe containing the ID_weights for all our cells
-
-
-## Latitude deviation (between outputs 2 by 2) ----------------------------------------------------------
-source("./scripts/data_analysis/lat_dev_2_by_2.R")
