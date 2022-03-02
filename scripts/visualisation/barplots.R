8f65b1d3b6f6c17944d9e24f35ae376420f42b4b
Buffan3369 <94531347+Buffan3369@users.noreply.github.com>
2022-03-02 12:22:03 +0100

    barplots for temporal evol of sd

---
+++ scripts/visualisation/barplots.R
@@ -0,0 +1,75 @@
+############ This script draws the barplots illustrating the distribution of cells in the different categories of lat sd ##########
+
+## import our friend ggplot ------------------------------------------------------------------
+library(ggplot2)
+library(viridis)
+
+
+## Read sd results and get rid of longitude (odd indexes) ------------------------------------
+sds <- readRDS("./data/standard_deviation_4mdls.RDS")
+sds <- sds[-MAX, -c(1:2, which(seq(from = 3, to = ncol(sds)+1, by = 1) %%2 != 0))] #MAX comes from the "cell_to_drop.R" script
+
+
+## Df for the barplot ------------------------------------------------------------------------
+
+final_df <- data.frame(TIME = rep(x = 0, 5),
+                       CAT = c("A: 0-5°", "B: 5-10°", "C: 10-20°", "D: 20-30°", "E: >30°"),
+                       COUNTS = c(29500, 0, 0, 0, 0))
+
+for(t in seq(from = 50, to = 500, by = 50)){
+  final_df <- rbind(final_df,
+                    data.frame(TIME = rep(x = -t, 5),
+                               CAT = c("A: 0-5°", "B: 5-10°", "C: 10-20°", "D: 20-30°", "E: >30°"),  #the five categories we're considering
+                               COUNTS = c(
+                                 length(which(sds[, t/10] < 5)),
+                                 length(which((sds[, t/10] < 10) & (sds[, t/10] >= 5))), #translating these categories in terms of logical condition
+                                 length(which((sds[, t/10] < 20) & (sds[, t/10] >= 10))),
+                                 length(which((sds[, t/10] < 30) & (sds[, t/10] >= 20))),
+                                 length(which(sds[, t/10] >= 30))
+                               )
+                    ))
+}
+
+## Plotting -----------------------------------------------------------------------------------
+
+  #raw counts
+barplt <- ggplot(data = final_df, aes(fill = CAT, x = TIME, y = COUNTS)) +
+  geom_bar(position = "stack", #display counts
+           stat = "identity") +
+  scale_fill_viridis(discrete = T) +
+  theme(axis.title.x = element_text(size = 18),
+        axis.title.y = element_text(size = 18),
+        axis.text = element_text(size = 15),
+        legend.title = element_text(size = 18),
+        legend.text = element_text(size = 15),
+        legend.key.size = unit(1, "cm"),
+        axis.line = element_line(colour = "black", size = 1, linetype = "solid"),
+  ) +
+  labs(x = "Time (x10Myr BP)", y = "Counts", fill = "Category") +
+  geom_vline(xintercept = -200, col = "black") +
+  geom_vline(xintercept = -410, col = "black")
+
+barplt #some time intervals seem to have disappearing cells that reappear aftewards. Let's do the same plot but with proportions
+ggsave(filename = "./figures/barplots/barplot_counts.pdf", plot = barplt)
+
+
+  #proportions
+bplt_prop <- ggplot(data = final_df, aes(fill = CAT, x = TIME, y = COUNTS)) +
+  geom_bar(position = "fill", #display proportions
+           stat = "identity") +
+  scale_fill_viridis(discrete = T) +
+  theme(axis.title.x = element_text(size = 18),
+        axis.title.y = element_text(size = 18),
+        axis.text = element_text(size = 15),
+        legend.title = element_text(size = 18),
+        legend.text = element_text(size = 15),
+        legend.key.size = unit(1, "cm"),
+        axis.line = element_line(colour = "black", size = 1, linetype = "solid"),
+        ) +
+  labs(x = "Time (x10Myr BP)", y = "Cell proportion", fill = "Category") +
+  geom_vline(xintercept = -200, col = "black") +
+  geom_vline(xintercept = -410, col = "black")
+
+
+bplt_prop
+ggsave(filename = "./figures/barplots/barplot_prop.pdf", plot = bplt_prop)
