b20fb0587c1bac3ddb42dc7e8ebec453aa66331a
Buffan3369 <94531347+Buffan3369@users.noreply.github.com>
2022-03-14 17:10:35 +0100

    changed export parameters

--- scripts/visualisation/barplots.R
+++ scripts/visualisation/barplots.R
@@ -49,12 +49,14 @@ barplt <- ggplot(data = final_df, aes(fill = CAT, x = TIME, y = COUNTS)) +
         panel.background = element_blank(), # Remove panel background
         panel.border = element_rect(colour = "black", fill = NA, size = 1) #frame the plot
         ) +
-  labs(x = "Time (Ma)", y = "Counts", fill = "Category") +
+  labs(x = "Time (Ma)", y = "Cell count", fill = "Category") +
   geom_vline(xintercept = 200, col = "red", linetype = "dashed", lwd = 1) +
-  geom_vline(xintercept = 410, col = "red", linetype = "dashed", lwd = 1)
+  geom_vline(xintercept = 410, col = "red", linetype = "dashed", lwd = 1) +
+  annotate(geom = "text", x = 205, y = 28000, label = "Seton Time Limit", angle = 90, size = 3, fontface = "italic", col = "red") +
+  annotate(geom = "text", x = 415, y = 28000, label = "Matthews Time Limit", angle = 90, size = 3, fontface = "italic", col = "red")
 
 print(barplt)
 
-ggsave(filename = "./figures/barplots/barplot_counts.pdf", plot = barplt) #save as pdf
-ggsave(filename = "./figures/barplots/barplot_counts.png", plot = barplt) #and png
+ggsave(filename = "./figures/barplots/barplot_counts.pdf", plot = barplt, width = 14, height = 7, units = "in") #save as pdf
+ggsave(filename = "./figures/barplots/barplot_counts.png", plot = barplt, width = 14, height = 7, units = "in") #and png
 
