14e16810de506f3550e10faf45445b38b8e0edae
Buffan3369 <94531347+Buffan3369@users.noreply.github.com>
2022-03-15 08:42:09 +0100

    ID_weight -> ID_score

--- scripts/MASTERFILES/data_analysis_MASTERFILE.R
+++ scripts/MASTERFILES/data_analysis_MASTERFILE.R
@@ -14,7 +14,7 @@ source("./scripts/data_analysis/lat_sd.R") # Latitude standard deviation
 source("./scripts/data_analysis/lat_dev_2_by_2.R") # Latitude deviation (between outputs 2 by 2)
 source("./scripts/data_analysis/MST.R") # MST computation (TAKES A WHILE.. should consider a parallelisation!!)
 
-source("./scripts/data_analysis/ID_weight.R") # Plate ID assignment discrepancies
+source("./scripts/data_analysis/ID_score.R") # Plate ID assignment discrepancies
 
 
 ###### CASE STUDY ######
