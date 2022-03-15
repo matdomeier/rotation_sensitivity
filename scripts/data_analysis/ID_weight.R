206f2ef5c51c6397714e4923b6b056f01cd5091b
Buffan3369 <94531347+Buffan3369@users.noreply.github.com>
2022-03-15 08:41:04 +0100

    changed name ID_weight -> ID_score

--- scripts/data_analysis/ID_weight.R
+++ scripts/data_analysis/ID_weight.R
@@ -13,10 +13,10 @@ colnames(store) <- c("lon_0", "lat_0", "Scotese2_ID", "Matthews_ID", "Golonka_ID
 
 store <- store[-MAX,] #we get rid of the points we don't want to work on (MAX defined in the "cells_to_drop.R" script)
 
-# Assess the ID_weight, the metric quantifying the number of different plate IDs a point has been assigned to according to the 3 models
-store$ID_weight <- 0
+# Assess the ID_score, the metric quantifying the number of different plate IDs a point has been assigned to according to the 3 models
+store$ID_score <- 0
 for(id in 1:nrow(store)){
-  store$ID_weight[id] <- length(unique(as.numeric(store[id,3:5])))  #the length of the plateID row without duplicates
+  store$ID_score[id] <- length(unique(as.numeric(store[id,3:5])))  #the length of the plateID row without duplicates
 }
 
 saveRDS(store, file = "./data/data_pts_plate_IDs_according_to_the_four_models.RDS")
