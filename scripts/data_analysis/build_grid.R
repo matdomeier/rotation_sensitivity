be68469fe4de32c018b9dbad2b454028249bbe08
Buffan3369 <94531347+Buffan3369@users.noreply.github.com>
2022-04-21 15:02:39 +0200

    data from the new grid re-extracted

--- scripts/data_analysis/build_grid.R
+++ scripts/data_analysis/build_grid.R
@@ -23,11 +23,11 @@ grid_shp <- shapefile("./data/meshgrid/icosahedral_grid.shp")
 
 ## get centroids of each cells as spatial data point ---------------------------
 spdp <- rgeos::gCentroid(grid_shp, byid = TRUE)
-df <- data.frame(spdp) #dataframe of the coordinates of the centroids
-df$Beginning <- 540
-df$End <- 0
-df <- SpatialPointsDataFrame(coords = df[,1:2], data = df)
-proj4string(df)<- CRS("+proj=longlat +datum=WGS84") #assign coord system to the SpatialPointsDataFrame
+xy.df <- data.frame(spdp) #dataframe of the coordinates of the centroids
+xy.df$Beginning <- 540
+xy.df$End <- 0
+xy.df <- SpatialPointsDataFrame(coords = xy.df[,1:2], data = xy.df)
+proj4string(xy.df)<- CRS("+proj=longlat +datum=WGS84") #assign coord system to the SpatialPointsDataFrame
 
 ## Eliminate points not covered by at least one model --------------------------
 
@@ -36,11 +36,11 @@ index_to_drop <- c()
 for(mdl in models){
   path <- paste0("./data/continental_polygons/", mdl, "/", mdl, "_PresentDay_ContinentalPolygons.shp")
   cont_pol <- shapefile(path) #we open the present-day continental polygons corresponding to the model
-  georef <- sp::over(df, cont_pol)$PLATEID1 #we overlay our spatial df with the continental polygons by plate ID
+  georef <- sp::over(xy.df, cont_pol)$PLATEID1 #we overlay our spatial xy.df with the continental polygons by plate ID
   index_to_drop <- c(index_to_drop, which(is.na(georef) == TRUE)) #we target the points not associated with any plate ID (that will therefore not be rotated)
 }
 index_to_drop <- unique(index_to_drop) #eliminate redundancy
-df <- df[-index_to_drop, ] #finally get rid of them
+xy.df <- xy.df[-index_to_drop, ] #finally get rid of them
 
 ## export the spatial points dataframe as a shapefile --------------------------
-raster::shapefile(df, "./data/meshgrid/meshgrid.shp", overwrite = TRUE)  #create shapefile from the SpatialPtsDF and save it with the same command
+raster::shapefile(xy.df, "./data/meshgrid/meshgrid.shp", overwrite = TRUE)  #create shapefile from the SpatialPtsDF and save it with the same command
