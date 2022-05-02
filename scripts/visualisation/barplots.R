cdb5b973063745c502273cba6d60010ee5f66022
Buffan3369 <94531347+Buffan3369@users.noreply.github.com>
2022-05-02 13:20:13 +0200

    adjust barplots

--- scripts/visualisation/barplots.R
+++ scripts/visualisation/barplots.R
@@ -75,8 +75,8 @@ BaRploTs <- function(metric){
     labs(x = "Time (Ma)", y = "Cell count", fill = "Category") +
     geom_vline(xintercept = 200, col = "red", linetype = "dashed", lwd = 1) +
     geom_vline(xintercept = 410, col = "red", linetype = "dashed", lwd = 1) +
-    annotate(geom = "text", x = 205, y = 24000, label = "Seton Time Limit", angle = 90, size = 3, fontface = "italic", col = "red") +
-    annotate(geom = "text", x = 415, y = 24000, label = "Matthews Time Limit", angle = 90, size = 3, fontface = "italic", col = "red")
+    annotate(geom = "text", x = 205, y = 23000, label = "Seton Time Limit", angle = 90, size = 3, fontface = "italic", col = "red") +
+    annotate(geom = "text", x = 415, y = 23000, label = "Matthews Time Limit", angle = 90, size = 3, fontface = "italic", col = "red")
   
   ggsave(filename = paste0("./figures/barplots/", metric, "_barplot_counts.pdf"), plot = barplt, width = 14, height = 7, units = "in") #save as pdf
   ggsave(filename = paste0("./figures/barplots/", metric, "_barplot_counts.png"), plot = barplt, width = 14, height = 7, units = "in") #and png
