385a97704828592e2727b5e6663ae8a10b16ec69
Buffan3369 <94531347+Buffan3369@users.noreply.github.com>
2022-05-02 13:01:05 +0200

    modify plot sd function

--- scripts/visualisation/plot_lat_sd.R
+++ scripts/visualisation/plot_lat_sd.R
@@ -1,52 +1,58 @@
-################ This script is to plot the latitude STANDARD DEVIATION between the four models over time ################
+library(dggridR)
+library(sp)
+library(maps)
+library(stars)
+library(sf)
 
+## Global initial grid ---------------------------------------------------------
+init_grid <- dgconstruct(spacing = 150)
 
-#Author: Lucas Buffan
-#Copyright (c) Lucas Buffan 2022
-#e-mail: lucas.l.buffan@gmail.com
+## Lat_sd df -------------------------------------------------------------------
+lat_sd <- readRDS("./data/lat_standard_deviation_4mdls.RDS")
 
+## Palette ---------------------------------------------------------------------
+pal <- c('#f7fcb9','#d9f0a3','#addd8e','#78c679','#41ab5d','#238443','#006837','#004529')
 
-library(raster)
+## Overlay cells of lat_sd df with cells of the initial grid -------------------
+  #(lat_0 and lon_0, the two first columns of lat_sd, are the coordinates on the present-day map of the rotated centroids of each cell of the grid)
+lat_sd$seqnum <- dgGEO_to_SEQNUM(dggs = init_grid, in_lon_deg = lat_sd$lon_0, in_lat_deg = lat_sd$lat_0)$seqnum
 
+## Build a grid with cells only containing a lat_sd value ----------------------
+grid <- dgcellstogrid(init_grid, lat_sd$seqnum)
 
-## Function to plot either lat or lon sd between the four models ---------------
-
-df <- readRDS("./data/lat_standard_deviation_4mdls.RDS")
-pal <- c('#f7fcb9','#d9f0a3','#addd8e','#78c679','#41ab5d','#238443','#006837','#004529') #set the palette
-
-
-plot_lat_sd <- function(){
-  for(k in seq(from = 3, to = ncol(df), by = 1)){
-    true_time <- (k-2)*10
-    xyz <- df[, c(1,2,k)] #select the corresponding deviation
-    r <- rasterFromXYZ(xyz, 
-                       crs = "+proj=moll +lon_0=0 +x_0=0 +y_0=0" )  #write the raster file with the mollweide projection coord sys
+## Loop to plot lat_sd over time out of this grid ------------------------------
+for(t in seq(from = 10, to = 540, by = 10)){
+  col <- paste0("lat_", t) #column name in lat_sd dataset
   
-    if(true_time < 100){  #add a zero in front of true_time in the name of the file so that the program used to compile the plot as a GIF could sort them properely
-      png(paste0("./figures/standard_deviation/", compound, "/", 0, true_time, ".png"))
+  #merge with the lat sd values of an arbitrary time (e.g. 100Ma)
+  grid1 <- merge(grid, lat_sd[,c("seqnum", col)], by = c("seqnum"))
+  
+  #getting rid of the cells that will be weirdly plotted (i.e that have wrapping-around coordinates, hence represented as straight east-west lines along the plot)
+  getout <- c()
+  for(index in 1:length(grid1$geometry)){
+    poly <- data.frame(grid1$geometry[[index]][1]) #dataframe containing the coordinates of the index-th polygon
+    if( abs(max(poly[,1]) - min(poly[,1])) > 300 ){ 
+      getout <- c(getout, index)
     }
-    else{
-      png(paste0("./figures/standard_deviation/", compound, "/",true_time, ".png"))
-    }
-    plot.new()
-    par(bg = "grey92")
-    plot(p,
-         axes = FALSE,
-         col = pal,
-         main = paste0("Latitudinal Standard Deviation between the models ", "(", true_time ,"Ma)"),
-         legend.args = list(text = 'Standard deviation (°)', side = 4, font = 2, line = 2.5, cex = 0.8),
-         zlim = c(0,60)
-         )  #display the output
-    plot(worldline_mol,
-         add = TRUE,
-         col = adjustcolor("grey30",alpha.f=0.5)) #add background map with a semi-transparent grey colour
-    dev.off()
   }
-  return()
+  
+  grid1$seqnum[getout] = NA
+  grid1[getout, col] = NA
+  grid1$geometry[getout] = NA
+  
+  if(t < 100){  #add a zero in front of tin the name of the file so that the program used to compile the plot as a GIF could sort them properely
+    pdf(paste0("./figures/standard_deviation/", 0, t, ".pdf"))
+  }
+  else{
+    pdf(paste0("./figures/standard_deviation/", t, ".pdf"))
+  }
+  
+  plot(grid1[col], 
+       pal = pal, 
+       breaks = seq(from = 0, to = 60, by = 7.5),
+       main = paste0("Latitudinal Standard Deviation between the models (", t, " Ma)"),
+       lwd = 0.05,
+       key.width = 0.1)
+  
+  dev.off()
 }
-
-
-
-## Execute ---------------------------------------------------------------------
-
-plot_lat_sd()
