3671ca5217947fd88a3933b4b91b7a82fc64c65e
Buffan3369 <94531347+Buffan3369@users.noreply.github.com>
2022-05-02 13:16:17 +0200

    fix typo

--- scripts/visualisation/barplots.R
+++ scripts/visualisation/barplots.R
@@ -34,7 +34,7 @@ BaRploTs <- function(metric){
   ## Df for the barplot ----------------------------------------------------------------------
   final_df <- data.frame(TIME = rep(x = 0, 5),
                          CAT = CAT,
-                         COUNTS = c(29500, 0, 0, 0, 0))
+                         COUNTS = c(25351, 0, 0, 0, 0))
   
   for(t in seq(from = 10, to = 540, by = 10)){
     final_df <- rbind(final_df,
@@ -75,8 +75,8 @@ BaRploTs <- function(metric){
     labs(x = "Time (Ma)", y = "Cell count", fill = "Category") +
     geom_vline(xintercept = 200, col = "red", linetype = "dashed", lwd = 1) +
     geom_vline(xintercept = 410, col = "red", linetype = "dashed", lwd = 1) +
-    annotate(geom = "text", x = 205, y = 28000, label = "Seton Time Limit", angle = 90, size = 3, fontface = "italic", col = "red") +
-    annotate(geom = "text", x = 415, y = 28000, label = "Matthews Time Limit", angle = 90, size = 3, fontface = "italic", col = "red")
+    annotate(geom = "text", x = 205, y = 24000, label = "Seton Time Limit", angle = 90, size = 3, fontface = "italic", col = "red") +
+    annotate(geom = "text", x = 415, y = 24000, label = "Matthews Time Limit", angle = 90, size = 3, fontface = "italic", col = "red")
   
   ggsave(filename = paste0("./figures/barplots/", metric, "_barplot_counts.pdf"), plot = barplt, width = 14, height = 7, units = "in") #save as pdf
   ggsave(filename = paste0("./figures/barplots/", metric, "_barplot_counts.png"), plot = barplt, width = 14, height = 7, units = "in") #and png
