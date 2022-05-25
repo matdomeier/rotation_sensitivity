1c4252583ea40e7743740d48fb9da36326f5ef21
Buffan3369 <94531347+Buffan3369@users.noreply.github.com>
2022-05-25 16:23:09 +0200

    Replace .jpg by .png (lighter)

--- scripts/visualisation/plot_lat_sd.R
+++ scripts/visualisation/plot_lat_sd.R
@@ -41,10 +41,10 @@ for(t in seq(from = 10, to = 540, by = 10)){
   grid1$geometry[getout] = NA
   
   if(t < 100){  #add a zero in front of tin the name of the file so that the program used to compile the plot as a GIF could sort them properely
-    jpeg(paste0("./figures/standard_deviation/", 0, t, ".png"), height = 230, width = 400, units = "mm", res = 800)
+    png(paste0("./figures/standard_deviation/", 0, t, ".png"), height = 230, width = 400, units = "mm", res = 800)
   }
   else{
-    jpeg(paste0("./figures/standard_deviation/", t, ".png"), height = 230, width = 400, units = "mm", res = 800)
+    png(paste0("./figures/standard_deviation/", t, ".png"), height = 230, width = 400, units = "mm", res = 800)
   }
   
   raster::plot(grid1[col], 
@@ -55,40 +55,3 @@ for(t in seq(from = 10, to = 540, by = 10)){
   
   dev.off()
 }
-
-
-
-
-
-
-
-
-
-
-
-t = 200
-col <- paste0("lat_", t) #column name in lat_sd dataset
-
-#merge with the lat sd values of an arbitrary time (e.g. 100Ma)
-grid1 <- merge(grid, lat_sd[,c("seqnum", col)], by = c("seqnum"))
-
-#getting rid of the cells that will be weirdly plotted (i.e that have wrapping-around coordinates, hence represented as straight east-west lines along the plot)
-getout <- c()
-for(index in 1:length(grid1$geometry)){
-  poly <- data.frame(grid1$geometry[[index]][1]) #dataframe containing the coordinates of the index-th polygon
-  if( abs(max(poly[,1]) - min(poly[,1])) > 300 ){ 
-    getout <- c(getout, index)
-  }
-}
-
-grid1$seqnum[getout] = NA
-grid1[getout, col] = NA
-grid1$geometry[getout] = NA
-
-jpeg(filename = "C:/Users/lucas/OneDrive/Bureau/try.jpg", height = 230, width = 400, units = "mm", res = 800)
-raster::plot(grid1[col], 
-     pal = pal, 
-     breaks = seq(from = 0, to = 60, by = 7.5),
-     main = paste0("Latitudinal Standard Deviation between the models (", t, " Ma)"),
-     cex.main = 1.5)
-dev.off()
