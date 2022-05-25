c02547d05cc3ff14c574838f0ee4cf7544e611cf
Buffan3369 <94531347+Buffan3369@users.noreply.github.com>
2022-05-25 11:11:42 +0200

    eliminate unuseful scripts

--- scripts/MASTERFILES/data_analysis_MASTERFILE.R
+++ scripts/MASTERFILES/data_analysis_MASTERFILE.R
@@ -11,7 +11,6 @@
 ## Build the equal-area grid that we are going to rotate ----------------------------------------------
 source("./scripts/data_analysis/build_grid.R")   #may take some time, maybe don't run
 
-
 # Rotating in Gplates, treatment of the outputs in the "rotating.R" file, raw outputs not joined in this repo as too heavy 
 
 
@@ -26,11 +25,9 @@ source("./scripts/data_analysis/MST.R") # MST computation (TAKES A WHILE.. shoul
 source("./scripts/data_analysis/prepare_fossil_reef_data.R") # cleaning corals data
 source("./scripts/data_analysis/prepare_fossil_croc_data.R") #cleaning crocs data
 source("./scripts/data_analysis/create_sfs.R") #prepare input shapefiles for Gplates
+
 # rotating in Gplates and output shapefiles not provided as too heavy: for each model and each taxon, the coordinates of the rotated occurrences are pooled in one RDS file
 # see "./scripts/data_analysis/extract_fossils_palaeocoordinates.R" for that
 
 ## Rotating fossil occurrences with Chronosphere package (instead of manually in Gplates) -------------
 source("./scripts/data_analysis/rotate_fossils_with_chronosphere.R")
-
-## quantify % occurrences in risky zones (high ID_sc) -------------------------------------------------
-source('./scripts/data_analysis/quantify_percent_occ_risky.R')
