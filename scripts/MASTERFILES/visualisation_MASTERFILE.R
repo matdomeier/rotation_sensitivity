a94df56db832d1a105d61b5cc3a627047c1a472f
Buffan3369 <94531347+Buffan3369@users.noreply.github.com>
2022-05-25 11:08:43 +0200

    eliminate functions no longer used

--- scripts/MASTERFILES/visualisation_MASTERFILE.R
+++ scripts/MASTERFILES/visualisation_MASTERFILE.R
@@ -2,25 +2,13 @@
 ######################################################## VISUALISATION #########################################################
 
 ###### SIMULATION PART ######
-## set the background map -------------------------------------------------------------
-source("./scripts/visualisation/background_map.R") #nb: ignore warnings
-
 ## Plot and save latitude standard deviation time series ------------------------------
 source("./scripts/visualisation/plot_lat_sd.R") #idem
-
-## Plot and save latitude deviation 2 by 2 for all the models time series -------------
-source("./scripts/visualisation/plot_lat_deviation.R")
 
 ## Plot and save the MST length assessment --------------------------------------------
 source("./scripts/visualisation/plot_MST.R")
 
     ### FOR THE SCRIPT TO WRITE THE GIFS, SEE THE PYTHON NOTEBOOK "make_GIFs.ipynb"
-
-## Plot and save plateIDs assignement discrepancies -----------------------------------
-source("./scripts/visualisation/plot_ID_score.R")
-
-## Plot and save time series for the three selected frames ----------------------------
-source("./scripts/visualisation/time_series_frames.R")
 
 ## Plot and save barplot ncells = f(t) ------------------------------------------------
 source("./scripts/visualisation/barplots.R")
