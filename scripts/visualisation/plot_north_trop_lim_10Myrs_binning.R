f67ab4d6ce816541d0efc2276c35d259eff698e6
Buffan3369 <94531347+Buffan3369@users.noreply.github.com>
2022-06-01 17:00:37 +0200

    esthetical changes in the case study figures

--- scripts/visualisation/plot_north_trop_lim_10Myrs_binning.R
+++ scripts/visualisation/plot_north_trop_lim_10Myrs_binning.R
@@ -9,8 +9,8 @@
 ## Libraries ---------------------------------------------------------------------------------------------
 library(ggplot2)
 library(rphylopic)
+library(ggpubr)
 
-palette <- c("#33a02c", "#fb9a99", "#e31a1c", "#fdbf6f")
 models <- c("Wright", "Seton", "Matthews", "Scotese")
 
 silhouettes <- c("d148ee59-7247-4d2a-a62f-77be38ebb1c7", #Cnidaria
@@ -23,16 +23,19 @@ make_plots <- function(taxon){ #taxon = Crocos or Corals
   
   if(taxon == "Crocos"){
     yrange <- c(20, 83)
-    phylo_coords <- c(-33, 80)
+    phylo_coords <- c(-30, 83)
     img <- image_data(silhouettes[[2]], size = 512)[[1]]
     fill_col <- "#41ab5d"
+    xlab = "Time (Ma)"
     tax <- "Terrestrial Crocodylomorphs"
-    }
+  }
+  
   else if(taxon == "Corals"){
     yrange <- c(10, 73)
     phylo_coords <- c(-8, 55)
     img <- image_data(silhouettes[[1]], size = 512)[[1]]
     fill_col <- "#ef6548"
+    xlab = NULL
     tax <- "warm-water Coral Reefs"
   }
   
@@ -40,8 +43,7 @@ make_plots <- function(taxon){ #taxon = Crocos or Corals
   coverage <- sort(unique(data_ex$TB))[-which(sort(unique(data_ex$TB)) > 200)] #we constrain ourselves to the early jurassic
   
   TIME <- c()
-  MAXI <- c() #these vectors will contain the max and min of the reconstructed latitudes according to the model => extent of intertropical belt
-  MINI <- c()
+  MAXI <- c() #this vectors will contain the max of the reconstructed latitudes IN ABSOLUTE VALUE according to each model. Assuming symmetry in tropics, this is going to return either the northern and the southern latitudinal limit of the tropics (abs)
   GROUP <- c()
   for(mdl in models){
     data <- readRDS(paste0("./data/fossil_extracted_paleocoordinates/", taxon, "/10Myrs_time_bins/", mdl, ".RDS"))
@@ -50,26 +52,37 @@ make_plots <- function(taxon){ #taxon = Crocos or Corals
     for(t in coverage){
       COL <- paste0("lat_", t)
       indexes <- which(is.na(data[, c(COL)]) == FALSE)
-      lat <- data[indexes, c(COL)]
+      lat <- abs(data[indexes, c(COL)])
       MAXI <- c(MAXI, max(lat))
-      MINI <- c(MINI, min(lat))
     }
   }
   to_plot <- data.frame(TIME = coverage,
-                        lat_max = MAXI,
-                        lat_min = MINI,
+                        lat_max_per_model = MAXI,
                         GROUP = GROUP)
   
-  ######################## Plot with the 4 models reconstruction of the Northern limit of the (sub)tropical zone (max lat occurrence)######################
-  northern_border <- ggplot(data = to_plot, aes(x = TIME, y = lat_max, group = GROUP, color = factor(GROUP))) +
-    geom_point() +
+  #get the max and min between each of the four characterised maxima to further plot the uncertainty zone as a ribbon
+  mx <- c()
+  mn <- c()
+  for(t in coverage){
+    indexes <- which(to_plot$TIME == t) #length 4 as 4 models
+    mx <- c(mx, max(to_plot$lat_max_per_model[indexes]))
+    mn <- c(mn, min(to_plot$lat_max_per_model[indexes]))
+  }
+  
+  to_plot$upper_boundary = rep(mx, 4)
+  to_plot$lower_boundary = rep(mn, 4)
+  
+  ######################## Plot the 4 models reconstruction of the Northern limit of the subtropical zone (max |lat| occurrence with uncertainty zone)######################
+  northern_border <- ggplot(data = to_plot, aes(x = TIME, y = lat_max_per_model, group = GROUP)) +
+    geom_ribbon(aes(ymin = lower_boundary, ymax = upper_boundary), fill = fill_col, alpha = 0.5)+
+    geom_point(aes(shape = factor(GROUP)), size = 3) +
+    geom_line(lwd = 0.5) +
     ggtitle(paste0("Northern subtropical limit based on ", tax)) +
+    add_phylopic(img, alpha = 1, x = phylo_coords[1], y = phylo_coords[2], ysize = 10)+
     scale_x_reverse(breaks = seq(from = 0, to = 200, by = 50)) +
     scale_y_continuous(limits = c(yrange[1], yrange[2]), 
                        breaks = seq(from = yrange[1]+10, to = yrange[2], by = 10), 
                        labels = seq(from = yrange[1]+10, to = yrange[2], by = 10))+
-    scale_colour_manual(values = palette, name = "Model") +    
-    geom_line(lwd = 1) +
     theme(text = element_text(size = 22),
           plot.title = element_text(size = 20),
           axis.text.x = element_text(size = 19),
@@ -77,85 +90,30 @@ make_plots <- function(taxon){ #taxon = Crocos or Corals
           panel.grid.major = element_blank(),
           panel.grid.minor = element_blank(), 
           panel.background = element_blank(),
-          panel.border = element_rect(colour = "black", fill = NA, size = 0.5)) + #frame the plot
-    labs(x = "Time (Ma)", y = "Palaeolatitude (°)") +
-    annotate("rect", xmin = Inf, xmax = 200, ymin = -Inf, ymax = yrange[1]+3, alpha = 1, color = "black", fill = "white")+
+          panel.border = element_rect(colour = "black", fill = NA, size = 2)) + #frame the plot
+    labs(x = xlab, y = "Absolute palaeolatitude (°)", shape = "Model") +
+    annotate("rect", xmin = Inf, xmax = 200, ymin = -Inf, ymax = yrange[1]+3, alpha = 1, color = "black", fill = "white", size = 1.5)+
     annotate("rect", xmin = 200, xmax = 145, ymin = -Inf, ymax = Inf, alpha = 0.2, fill = "grey40")+
-    annotate("rect", xmin = 200, xmax = 145, ymin = -Inf, ymax = yrange[1]+3, alpha = 1, color = "black", fill = "white")+
-    annotate("text", x = (200+145)/2, y = yrange[1]+0.5, label = "Ju", size = 7)+
-    annotate("rect", xmin = 145, xmax = 66, ymin = -Inf, ymax = yrange[1]+3, alpha = 1, color = "black", fill = "white")+
-    annotate("text", x = (145+66)/2, y = yrange[1]+0.5, label = "Cr", size = 7)+
+    annotate("rect", xmin = 200, xmax = 145, ymin = -Inf, ymax = yrange[1]+3, alpha = 1, color = "black", fill = "white", size = 1.5)+
+    annotate("text", x = (200+145)/2, y = yrange[1]+0.5, label = "J", size = 8)+
+    annotate("rect", xmin = 145, xmax = 66, ymin = -Inf, ymax = yrange[1]+3, alpha = 1, color = "black", fill = "white", size = 1.5)+
+    annotate("text", x = (145+66)/2, y = yrange[1]+0.5, label = "K", size = 8)+
     annotate("rect", xmin = 66, xmax= 23.03, ymin = -Inf, ymax = Inf, alpha = 0.2, fill = "grey40")+
-    annotate("rect", xmin = 66, xmax = 23.03, ymin = -Inf, ymax = yrange[1]+3, alpha = 1, color = "black", fill = "white")+
-    annotate("text", x = (66+23.03)/2, y = yrange[1]+0.5, label = "Pg", size = 7)+
-    annotate("rect", xmin = 23.03, xmax = 2.58, ymin = -Inf, ymax = yrange[1]+3, alpha = 1, color = "black", fill = "white")+
-    annotate("text", x = (23.03+2.58)/2, y = yrange[1]+0.5, label = "Ng", size = 7)+
-    annotate("rect", xmin = 2.58, xmax = -Inf, ymin = -Inf, ymax = yrange[1]+3, alpha = 1, color = "black", fill = "white")
+    annotate("rect", xmin = 66, xmax = 23.03, ymin = -Inf, ymax = yrange[1]+3, alpha = 1, color = "black", fill = "white", size = 1.5)+
+    annotate("text", x = (66+23.03)/2, y = yrange[1]+0.5, label = "Pg", size = 8)+
+    annotate("rect", xmin = 23.03, xmax = 2.58, ymin = -Inf, ymax = yrange[1]+3, alpha = 1, color = "black", fill = "white", size = 1.5)+
+    annotate("text", x = (23.03+2.58)/2, y = yrange[1]+0.5, label = "Ng", size = 8)+
+    annotate("rect", xmin = 2.58, xmax = -Inf, ymin = -Inf, ymax = yrange[1]+3, alpha = 1, color = "black", fill = "white", size = 1.5)
   
-  ggsave(filename = paste0("./figures/case_study/", taxon, "/lat_north_bound_4mdl_10Myrs_bins.pdf"),
-         plot = northern_border,
-         height = 5,
-         width = 9)
-  
-  ####################### Now, constrain to represent the min and max of each slice ######################
-  mx <- c()
-  mn <- c()
-  md <- c()
-  for(t in coverage){
-    indexes <- which(to_plot$TIME == t)
-    mx <- c(mx, max(to_plot$lat_max[indexes]))
-    mn <- c(mn, min(to_plot$lat_max[indexes]))
-    md <- c(md, median(to_plot$lat_max[indexes]))
-  }
-  
-  tax_range <- data.frame(time = coverage,
-                           max = mx,
-                           min = mn,
-                          median = md)
-  
-  dist_plot <- ggplot(data = tax_range, aes(x = time, y = mx)) +
-    scale_x_reverse(breaks = seq(from = 0, to = 200, by = 50))+
-    geom_line(aes(y = mx), linetype = "solid", lwd = 1.2)+
-    geom_line(aes(y = mn), linetype = "solid", lwd = 1.2)+
-    geom_line(aes(y = md), linetype = "dashed", lwd = 1) +
-    geom_ribbon(aes(ymin = mn, ymax = mx), fill = fill_col, alpha = 0.7)+
-    scale_y_continuous(limits = c(yrange[1], yrange[2]), 
-                       breaks = seq(from = yrange[1]+10, to = yrange[2], by = 10), 
-                       labels = seq(from = yrange[1]+10, to = yrange[2], by = 10))+
-    ggtitle(paste0("Northern subtropical limit based on ", tax))+
-    add_phylopic(img, alpha = 1, x = phylo_coords[1], y = phylo_coords[2], ysize = 10)+
-    theme(text = element_text(size = 25),
-          plot.title = element_text(size = 20),
-          axis.text.x = element_text(size = 23),
-          axis.text.y = element_text(size = 23),
-          panel.grid.major = element_blank(), # Remove panel grid lines
-          panel.grid.minor = element_blank(), 
-          panel.background = element_blank(), # Remove panel background
-          panel.border = element_rect(colour = "black", fill = NA, size = 1))+
-    labs(x = "Time (Ma)", y= "Palaeolatitude (°)") +
-    annotate("rect", xmin = Inf, xmax = 200, ymin = -Inf, ymax = yrange[1]+3, alpha = 1, color = "black", fill = "white")+
-    annotate("rect", xmin = 200, xmax = 145, ymin = -Inf, ymax = Inf, alpha = 0.2, fill = "grey40")+
-    annotate("rect", xmin = 200, xmax = 145, ymin = -Inf, ymax = yrange[1]+3, alpha = 1, color = "black", fill = "white")+
-    annotate("text", x = (200+145)/2, y = yrange[1]+0.5, label = "J", size = 7)+
-    annotate("rect", xmin = 145, xmax = 66, ymin = -Inf, ymax = yrange[1]+3, alpha = 1, color = "black", fill = "white")+
-    annotate("text", x = (145+66)/2, y = yrange[1]+0.5, label = "K", size = 7)+
-    annotate("rect", xmin = 66, xmax= 23.03, ymin = -Inf, ymax = Inf, alpha = 0.2, fill = "grey40")+
-    annotate("rect", xmin = 66, xmax = 23.03, ymin = -Inf, ymax = yrange[1]+3, alpha = 1, color = "black", fill = "white")+
-    annotate("text", x = (66+23.03)/2, y = yrange[1]+0.5, label = "Pg", size = 7)+
-    annotate("rect", xmin = 23.03, xmax = 2.58, ymin = -Inf, ymax = yrange[1]+3, alpha = 1, color = "black", fill = "white")+
-    annotate("text", x = (23.03+2.58)/2, y = yrange[1]+0.5, label = "Ng", size = 7)+
-    annotate("rect", xmin = 2.58, xmax = -Inf, ymin = -Inf, ymax = yrange[1]+3, alpha = 1, color = "black", fill = "white")
-  
-  ggsave(filename = paste0("./figures/case_study/", taxon, "/lat_north_bound_deviation_10Myrs_bins.pdf"),
-         plot = dist_plot,
-         height = 5,
-         width = 9)
+  return(northern_border)
   
 }
 
 
 ## EXECUTION --------------------------------------------------------------------------------------------------------
 
-for(taxon in c("Corals", "Crocos")){
-  make_plots(taxon)
-}
+cor_bor <- make_plots("Corals")
+cro_bor <- make_plots("Crocos")
+
+arr1 <- ggarrange(cor_bor, cro_bor, nrow = 2, ncol = 1, labels = "AUTO", font.label = list(size = 30))
+ggsave(filename = "./figures/case_study/subtrop_bound_estim_2taxa.png", plot = arr1, device = "png", width = 350, height = 350, unit = "mm")
