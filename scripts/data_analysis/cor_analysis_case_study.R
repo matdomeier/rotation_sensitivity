f23aeb0063e413797106f741e061f2781e18bc9c
Buffan3369 <94531347+Buffan3369@users.noreply.github.com>
2022-08-25 10:49:40 +0200

    added max absolute lat and p value

--- scripts/data_analysis/cor_analysis_case_study.R
+++ scripts/data_analysis/cor_analysis_case_study.R
@@ -9,7 +9,7 @@
 models <- c("Wright", "Seton", "Matthews", "Scotese")
 
 
-get_max_tax <- function(taxon){
+get_max_tax <- function(taxon){ #returns a table with max absolute palaeolatitude for each time step (each series of each model will be further analysed in the correlatio analysis)
   data_ex <- readRDS(paste0("./data/fossil_extracted_paleocoordinates/", taxon, "/10Myrs_time_bins/Scotese.RDS"))
   MAXI <- data.frame(time = sort(unique(data_ex$TB))[-which(sort(unique(data_ex$TB)) > 200)])
   for(mdl in models){
@@ -19,7 +19,7 @@ get_max_tax <- function(taxon){
       COL <- paste0("lat_", t)
       indexes <- which(is.na(data[, c(COL)]) == FALSE)
       lat <- abs(data[indexes, c(COL)])
-      maxi <- c(maxi, max(lat))
+      maxi <- c(maxi, max(abs(lat)))
     }
     MAXI[, c(mdl)] <- maxi
   }
@@ -27,7 +27,7 @@ get_max_tax <- function(taxon){
 }
 
 
-get_cor_tax <- function(taxon){
+get_cor_tax <- function(taxon, metric){ #correlation analysis out of the list of the max abs lat assessed in the previous function
   df <- get_max_tax(taxon)
   COR <- data.frame(Wright = rep(0,4),
                     Seton = rep(0,4),
@@ -37,7 +37,15 @@ get_cor_tax <- function(taxon){
   for(mdl in models){
     cor_mod <- c()
     for(mdl1 in models){
-      cor_mod <- c(cor_mod, cor.test(x = df[, c(mdl)], y = df[, c(mdl1)], method = "pearson")$estimate)
+      if(metric == "r"){ #retrun 
+        cor_mod <- c(cor_mod, cor.test(x = df[, c(mdl)], y = df[, c(mdl1)], method = "pearson")$estimate)
+      }
+      else if(metric == "p"){
+        cor_mod <- c(cor_mod, cor.test(x = df[, c(mdl)], y = df[, c(mdl1)], method = "pearson")$p.value)
+      }
+      else{
+        warning("metric has to be either 'r' (Pearson's R) or 'p' (p-value) ")
+      }
     }
     COR[, c(mdl)] <- cor_mod
   }
