7dd208385797efebeb44319e4e0db198175da380
Buffan3369 <lucas.buffan@ens-lyon.fr>
2022-12-28 19:10:58 +0100

    save max lat tab for correlation analysis

---
+++ scripts/data_analysis/07_cor_mat.R
@@ -0,0 +1,5 @@
+# ------------------------------------------------------------------------- #
+# Purpose: Assess how consistent together are fossil-inferred palaeolatitudinal limits of the subtropics between the models
+# Author(s): Lucas Buffan & Lewis A. Jones
+# Email: Lucas.L.Buffan@gmail.com; LewisAlan.Jones@uvigo.es
+# Load libraries ----------------------------------------------------------
